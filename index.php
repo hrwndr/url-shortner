3d776adbce6220f7df478e7bff915df35683d877
hrwndr <hrwndr97@gmail.com>
2017-09-24 16:58:31 +0530

    URL Shortener

--- index.php
+++ index.php
@@ -12,11 +12,12 @@ Github/hrwndr
         <title><?php echo $sitename; ?> | URL SHORTNER</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/gh/hrwndr/animate@v1.1/animate.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="style.css" />
         <link href="https://fonts.googleapis.com/css?family=Seaweed+Script" rel="stylesheet" />
         <link rel="icon" href="https://i.imgur.com/MZ8k2Dp.png" />
     </head>
-    <body>
+    <body class="fade-in-ii">
         <form class="form-wrapper cf" method="post" action="" >
             <font size="8" style="font-family: 'Seaweed Script', cursive;" alt="URL SHORTNER by HRWNdR"><?php echo $sitename; ?></font><p />
             <input type="text" class="textarea" name="textarea" placeholder="Enter Your Link Here..." required>
