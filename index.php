25f72db0da281d9abb3916ad0d2c3db784e36a17
hrwndr <hrwndr97@gmail.com>
2017-09-24 23:29:55 +0530

    URL Shortener

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ Github/hrwndr
     <head>
         <title><?php echo $sitename; ?> | URL SHORTNER</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#000000">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/gh/hrwndr/animate@v1.1/animate.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="style.css" />
